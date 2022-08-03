1babdaa2e9d9715f7d5cf467c85c56fc274bb33b
Danrlei Huber <darleirafaelhuber@gmail.com>
2022-08-03 19:05:37 -0300

    adicionado CRUD U e D

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import listaPessoaView, PessoaCreateView
+from .views import listaPessoaView, PessoaCreateView, PessoaUpdateView, PessoaDeleteView
 
 urlpatterns = [
     path('', listaPessoaView.as_view(),name='pessoa.index'),
-    path('novo/',PessoaCreateView.as_view(), name='pessoa.novo')
+    path('novo/',PessoaCreateView.as_view(), name='pessoa.novo'),
+    path('editar/<int:pk>',PessoaUpdateView.as_view(), name='pessoa.editar'),
+    path('remover/<int:pk>',PessoaDeleteView.as_view(), name='pessoa.remover'),
 ]
