4292bc3db0697fcf63a877f1e19db1713e3e7f73
Danrlei Huber <darleirafaelhuber@gmail.com>
2022-08-04 01:19:08 -0300

    adicionado funcionalidades ao django admin

--- pessoa/admin.py
+++ pessoa/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Pessoa, Contato
 
-# Register your models here.
+@admin.action(description='ativar todas as pessoas')
+def ativar_todos(modeladmin,request,queryset):
+    queryset.update(ativo=True)
+
+@admin.action(description='desativar todas as pessoas')
+def desativar_todos(modeladmin,request,queryset):
+    queryset.update(ativo=False)
+
+
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = [
+        'nome_completo',
+        'data_nascimento',
+        'ativo'
+    ]
+    list_filter = [
+        'ativo',
+        'data_nascimento'
+    ]
+    search_fields = [
+        'nome_completo'
+    ]
+    actions = [
+        ativar_todos,
+        desativar_todos
+    ]
+
+admin.site.register(Pessoa, PessoaAdmin)
+admin.site.register(Contato)
